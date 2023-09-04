588fe0bdd18d88ed81ec7654eaaad395e13b9fa9
Xau <frozzy.xfs@gmail.com>
2023-09-03 22:08:13 -0400

    Add helper function to calculate best weather condition to display

--- src/index.js
+++ src/index.js
@@ -43,8 +43,58 @@ function setPrimaryData(object, response) {
     return data;
 }
 
+// https://openweathermap.org/weather-conditions
+// Thunderstorm (2XX) > Snow (6XX) > Rain (5XX) > Atmospheric (7XX) > Drizzle (3XX) > Cloud (80X) > Clear (800)
+// p = 6 (Thunderstorm) -> 0 (Clear)
+// Condition = [code, description, icon]
 function calculateCondition(currentCondition, newCondition) {
-    let results = currentCondition;
+    let results;
+
+    // Check current conditions incase of reset
+    if (currentCondition !== undefined) {
+        const conditions = [currentCondition, newCondition];
+
+        const p = [0, 0];
+
+        for (let i = 0; i < conditions.length; i++) {
+            // First digit of code
+            const code = parseInt(conditions[i][0] / 100, 10);
+
+            switch (code) {
+                case 2:
+                    p[i] = 6;
+                    break;
+                case 6:
+                    p[i] = 5;
+                    break;
+                case 5:
+                    p[i] = 4;
+                    break;
+                case 7:
+                    p[i] = 3;
+                    break;
+                case 3:
+                    p[i] = 2;
+                    break;
+                case 8:
+                    if (conditions[i][0] !== 800) {
+                        p[i] = 1;
+                        break;
+                    }
+                // eslint-disable-next-line no-fallthrough
+                default:
+                    p[i] = 0;
+            }
+
+        }
+
+        if (p[1] > p[0])
+            results = [...newCondition];
+        else
+            results = [...currentCondition];
+    }
+    else
+        results = [...newCondition];
 
     return results;
 }
@@ -63,8 +113,7 @@ function parseForecastData(response) {
 
     let tempMin = response.list[0].main.temp_min;
     let tempMax = response.list[0].main.temp_max;
-    let condition = response.list[0].weather[0].description;
-    let icon = response.list[0].weather[0].icon;
+    let condition = [response.list[0].weather[0].id, response.list[0].weather[0].description, response.list[0].weather[0].icon];
     let humidity = response.list[0].main.humidity;
     let precipitation = response.list[0].pop;
     let count = 1;
@@ -86,13 +135,12 @@ function parseForecastData(response) {
             itemData.humidity = humidity / count;
             itemData.precipitation = precipitation;
 
-            const status = new Weather(month, day, condition, icon, itemData);
+            const status = new Weather(month, day, condition[1], condition[2], itemData);
             list.push(status);
 
             tempMin = 100;
             tempMax = -100;
-            condition = '';
-            icon = '';
+            condition = [];
             humidity = 0;
             precipitation = 0;
 
@@ -109,14 +157,14 @@ function parseForecastData(response) {
         humidity += item.main.humidity;
         precipitation += item.pop;
 
-        // Todo: calculate display of weather condition/icon
-
+        const newCondition = [item.weather[0].id, item.weather[0].description, item.weather[0].icon];
+        condition = calculateCondition(condition, newCondition);
 
         count += 1;
         index += 1;
     }
-
-    console.log(data);
+    
+    data.list = list;
 
     return data;
 }
@@ -133,7 +181,7 @@ async function getForecast(lat, lon) {
 
         data = parseForecastData(response);
 
-        console.log(response);
+        console.log(data);
     }
     catch (err) {
         document.querySelector('#content').textContent = 'Error reading forecast data';
@@ -167,7 +215,7 @@ async function test() {
 
     const data = parseForecastData(response);
 
-    // console.table(data);
+    console.log(data);
 }
 
 // setup();
